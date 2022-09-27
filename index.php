33ce902d476f66a3c0d88fddb47fcdf8a47d99c2
Sutsikii <nicolas-dutertre@bbox.fr>
2022-09-27 18:36:54 +0200

    Fixed-again

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ $form = new Form(array(
 //     die();
 
 echo("Hello webhook");
-echo("Users Rules fixed");
-echo("Users Rules again...");
-echo("Loosing hope");
-echo("Just a linux bug !");
-echo("Haha its was !");
+
 echo $form->input('username');
 echo $form->submit();

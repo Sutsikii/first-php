1ae93aa8e86c3b65d6db565958750137dc131e9c
Sutsikii <nicolas-dutertre@bbox.fr>
2022-09-27 20:15:24 +0200

    Fixed-again

--- index.php
+++ index.php
@@ -6,12 +6,5 @@ $form = new Form(array(
     'username' => 'Roger'
 ));
 
-// var_dump($form);
-//     die();
-
-echo("Il a dit c'est bon ( je le crois pas )");
-echo("Un retard?");
-
-
 echo $form->input('username');
 echo $form->submit();
